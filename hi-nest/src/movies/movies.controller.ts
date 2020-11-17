2b48c302dac66def4432ab97b6d29f498cdf8d4b
김윤수 <ys9922@naver.com>
2020-11-17 16:48:54 +0900

    NEST API

---
+++ hi-nest/src/movies/movies.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear}`;
+  // }
+
+  @Get(':id')
+  getOne(@Param('id') id: number): Movie {
+    return this.moviesService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() data: CreateMovieDto) {
+    return this.moviesService.create(data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') id: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(id, updateData);
+  }
+}
